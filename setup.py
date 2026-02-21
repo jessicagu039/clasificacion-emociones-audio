6bee2f59279a4bb1503290528c313c69dac2e3b7
jessicagu039 <tique.yessicaadriana@gmail.com>
2026-02-21 14:44:31 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Implementación de un sistema de reconocimiento automático de emociones en voz basado en el análisis de señales de audio. El proyecto utiliza la Mexican Emotional Speech Database (MESD) junto con técnicas de extracción de características acústicas y modelos de aprendizaje automático para la clasificación de emociones primarias.',
+    author='jessicagu029',
+    license='MIT',
+)
